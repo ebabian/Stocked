8d69c7f1145269e9e4b447caaffccfe335155f69
Elektra Babian <elektrababian@Elektras-MBP.hitronhub.home>
2020-06-08 11:58:27 -0400

    Added icon for delete button

--- models/inventory.js
+++ models/inventory.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const inventorySchema = new mongoose.Schema({
     name:  { type: String, required:true },
     type: { type: String, required:true } ,
+    Qty: Number,
     expiration: String,
     readyToEat: Boolean
 });
