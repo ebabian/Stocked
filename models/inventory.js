6d651f373be0e1f992ad762266d667f7d817b9d8
Elektra Babian <elektrababian@Elektras-MBP.hitronhub.home>
2020-06-08 15:35:10 -0400

    Added iframe for recipe links

--- models/inventory.js
+++ models/inventory.js
@@ -5,8 +5,11 @@ const inventorySchema = new mongoose.Schema({
     name:  { type: String, required:true },
     type: { type: String, required:true } ,
     qty: Number,
+    price: String,
     expiration: String,
-    url: String
+    url: String,
+    url2: String,
+    url3: String,
 });
 
 const Item = mongoose.model('Item', inventorySchema);
